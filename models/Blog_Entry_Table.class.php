c470d4f0448d123767c0a39a97163a6109ddb473
SerZile <ivanzivankic@ymail.com>
2014-12-10 22:30:21 +0100

    Characters are used as placeholders in the SQL string. An array is created with the dynamic data. The order of items must match the order used in the SQL string. Pass the array with dynamic data as an argument to the execute() method.

--- models/Blog_Entry_Table.class.php
+++ models/Blog_Entry_Table.class.php
@@ -8,10 +8,13 @@ class Blog_Entry_Table {
 	}
 
 	public function saveEntry ($title, $entry){
-		$entrySQL = "INSERT INTO blog_entry (title, entry_text) VALUES ('$title', '$entry')";
+		$entrySQL = "INSERT INTO blog_entry (title, entry_text) VALUES (?, ?)";
 		$entryStatement = $this->db->prepare($entrySQL);
+		
+		$formData = array($title, $entry);
+
 		try{
-			$entryStatement->execute();
+			$entryStatement->execute($formData);
 		} catch (Exception $e){
 			$msg = "<p>You tried to run this sql: $entrySQL</p>
 					<p>Exception: $e</p>";
